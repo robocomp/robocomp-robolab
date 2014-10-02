c4016c5c278e54d93c4e28905c0f6828e438879b
Luis J. Manso <luis.manso@gmail.com>
2014-10-02 12:44:56 +0200

    remove wrong openni component

--- components/openNIComp/bin/startDebugOpenNIComp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./opennicomp --Ice.Config=../etc/generic_config.debug, ../etc/specific_config.debug &
--- components/openNIComp/bin/startOpenNIComp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./opennicomp --Ice.Config=../etc/generic_config, ../etc/specific_config &

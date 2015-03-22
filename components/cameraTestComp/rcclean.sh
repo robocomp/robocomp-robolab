8b6cd455df61a2d3ed5eb4b8b1d6f7c8ec7b56b9
pbustos <pbustos@unex.es>
2015-03-22 22:37:08 +0100

    cameraTestCpp new component requiring RGBDBus interface

---
+++ components/cameraTestComp/rcclean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make clean
+rm Makefile
+rm -r CMakeFiles
+rm CMakeCache.txt
+rm *.kdev4
+rm cmake_install.cmake
+rm src/moc*
+rm src/Makefile
+rm -r src/CMakeFiles
+rm src/cmake_install.cmake
+
+
+

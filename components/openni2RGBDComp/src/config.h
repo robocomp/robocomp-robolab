d72e37bb40da9fb813f919c6bf55373f7b661bc9
pbustos <pbustos@unex.es>
2015-04-17 00:48:39 +0200

    adding openniRGB

---
+++ components/openni2RGBDComp/src/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Comment out this line if your application has a QtGui
+
+#define PROGRAM_NAME    " OpenNI2RGBDComp"
+#define SERVER_FULL_NAME   "RoboComp OpenNI2RGBDComp:: OpenNI2RGBDComp"
+
+#endif

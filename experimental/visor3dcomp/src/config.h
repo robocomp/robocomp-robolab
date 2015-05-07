be34801c208562b1aaed12a92c38ba99a639b92d
pbustos <pbustos@unex.es>
2015-05-08 00:51:13 +0200

    adding visor3D

---
+++ experimental/visor3dcomp/src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Comment out this line if your application has a QtGui
+#define USE_QTGUI
+
+
+#define PROGRAM_NAME    " Visor3D"
+#define SERVER_FULL_NAME   "RoboComp Visor3D:: Visor3D"
+
+#endif

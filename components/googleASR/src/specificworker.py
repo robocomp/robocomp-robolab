84011cb850b8698a0c83dd496f87b4fcb8c5f9c3
Luis J. Manso <luis.manso@gmail.com>
2015-05-13 11:50:16 +0200

    Added GoogleASR and GoogleASRClient

---
+++ components/googleASR/src/specificworker.py
@@ -0,0 +1,67 @@
+#
+# Copyright (C) 2015 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys, os, Ice, traceback
+
+import time
+
+from PySide import *
+from genericworker import *
+
+ROBOCOMP = ''
+try:
+	ROBOCOMP = os.environ['ROBOCOMP']
+except:
+	pass
+if len(ROBOCOMP)<1:
+	print 'ROBOCOMP environment variable not set! Exiting.'
+	sys.exit()
+
+
+preStr = "-I"+ROBOCOMP+"/interfaces/ --all "+ROBOCOMP+"/interfaces/"
+Ice.loadSlice(preStr+"ASRPublish.ice")
+from RoboCompASRPublish import *
+
+
+
+class SpecificWorker(GenericWorker):
+	def __init__(self, proxy_map):
+		super(SpecificWorker, self).__init__(proxy_map)
+		self.timer.timeout.connect(self.compute)
+		self.Period = 2000
+		self.timer.start(self.Period)
+		self.number = 0
+	def setParams(self, params):
+		return True
+
+	@QtCore.Slot()
+	def compute(self):
+		print 'SpecificWorker.compute...'
+		try:
+			self.asrpublish.newText(str(self.number))
+		except Ice.Exception, e:
+			traceback.print_exc()
+			print e
+		self.number += 1
+		time.sleep(1)
+
+
+
+
+

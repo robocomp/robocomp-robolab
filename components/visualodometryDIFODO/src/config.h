7254d172371d2b292c910149190c533c33e34de0
pbustos <pbustos@unex.es>
2015-06-14 18:46:38 +0200

    new visualodometryComp from the MRPT toolkit

---
+++ components/visualodometryDIFODO/src/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Comment out this line if your application has a QtGui
+
+#define PROGRAM_NAME    " visualodometryDIFODO"
+#define SERVER_FULL_NAME   "RoboComp visualodometryDIFODO:: visualodometryDIFODO"
+
+#endif

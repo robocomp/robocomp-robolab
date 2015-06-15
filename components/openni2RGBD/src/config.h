ce23f27aa7c5c9b676c149013daf4c2e41e87e52
mhaut <juanmariohaut@unex.es>
2015-06-15 21:08:14 +0200

    fix error in config.h

--- components/openni2RGBD/src/config.h
+++ components/openni2RGBD/src/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // Comment out this line if your application has a QtGui
-#define USE_QTGUI
+//#define USE_QTGUI
 
 #define PROGRAM_NAME    " openNI2Comp"
 #define SERVER_FULL_NAME   "RoboComp openNI2Comp:: openNI2Comp"

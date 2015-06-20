312f0f3d7373a28390abeecbf0ebaf4ea20ebd29
Daniel1108 <dgallegos@alumnos.unex.es>
2015-06-20 10:06:38 +0200

    more debug

--- experimental/CGR/src/specificworker.cpp
+++ experimental/CGR/src/specificworker.cpp
@@ -429,21 +429,6 @@ void SpecificWorker::drawLines()
 			i++;                        
 		}
 	}
-	int contador=0;
-	RoboCompLaser::TLaserData laserData;
-	laserData = laser_proxy->getLaserData();
-        for(uint i=0;i<laserData.size();i++)
-        {
-		if(contador>=100 and contador<=668)
-		{
-			const QString item = QString::fromStdString("laserPoint_")+QString::number(i);
-			const QString transf = QString::fromStdString("laserPointTransf_")+QString::number(i);
-			InnerModelDraw::addTransform(innerModelViewer,transf,"laserPose");
-			InnerModelDraw::addPlane_notExisting(innerModelViewer, item,transf,QVec::vec3(0,0,0),QVec::vec3(0,1,0),"#FFFFFF",QVec::vec3(50, 50, 50));
-		}
-		contador++;
-	}
-
 }
 void SpecificWorker::drawParticles()
 {
@@ -459,6 +444,22 @@ void SpecificWorker::drawParticles()
 	InnerModelDraw::addTransform(innerModelViewer,"redTransform","floor");
 	InnerModelDraw::addPlane_notExisting(innerModelViewer,"red","redTransform",QVec::vec3(0,0,0),QVec::vec3(1,0,0),"#AA0000",QVec::vec3(200, 1000, 200));
 	InnerModelDraw::addPlane_notExisting(innerModelViewer, "orientacion","redTransform",QVec::vec3(0,500,200),QVec::vec3(1,0,0),"#00FF00",QVec::vec3(200, 50, 50));
+
+	//Draw Laser
+	int contador=0;
+	RoboCompLaser::TLaserData laserData;
+	laserData = laser_proxy->getLaserData();
+        for(uint i=0;i<laserData.size();i++)
+        {
+		if(contador>=100 and contador<=668)
+		{
+			const QString item = QString::fromStdString("laserPoint_")+QString::number(i);
+			const QString transf = QString::fromStdString("laserPointTransf_")+QString::number(i);
+			InnerModelDraw::addTransform(innerModelViewer,transf,"redTransform");
+			InnerModelDraw::addPlane_notExisting(innerModelViewer, item,transf,QVec::vec3(0,0,0),QVec::vec3(0,1,0),"#FFFFFF",QVec::vec3(50, 50, 50));
+		}
+		contador++;
+	}
 }
 
 
@@ -513,7 +514,7 @@ void SpecificWorker::updateLaser()
 		{
 			const QString transf = QString::fromStdString("laserPointTransf_")+QString::number(cont);
 			lidarParams.laserScan[j] = i.dist/1000.f;
-			innerModel->updateTransformValues(transf, i.dist*sin(i.angle), 0, i.dist*cos(i.angle), 0, 0, 0, "laserPose");
+			innerModel->updateTransformValues(transf, i.dist*sin(i.angle), 0, i.dist*cos(i.angle), 0, 0, 0, "redTransform");
 			j++;
 		}
 		cont++;

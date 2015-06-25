4125e485fdb31b515719d487e140a67f6cf9c2da
mhaut <juanmariohaut@unex.es>
2015-06-25 23:45:11 +0200

    new component localization CGR with OpenNI

---
+++ experimental/CGRcomplete/src/specificworker.h
@@ -0,0 +1,85 @@
+/*
+ *    Copyright (C) 2015 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+       \brief
+       @author authorname
+*/
+
+#ifndef SPECIFICWORKER_H
+#define SPECIFICWORKER_H
+
+#include "vectorparticlefilter.h"
+#include "plane_filtering.h"
+#include <genericworker.h>
+#include <innermodel/innermodel.h>
+#include <osgviewer/osgview.h>
+#include "innermodeldraw.h"
+#include <innermodel/innermodelviewer.h>
+
+class SpecificWorker : public GenericWorker
+{
+Q_OBJECT
+public:
+	SpecificWorker(MapPrx& mprx);	
+	~SpecificWorker();
+	bool setParams(RoboCompCommonBehavior::ParameterList params);
+        void LoadParameters();
+	void newFilteredPoints(const OrientedPoints &ops);
+	void filterParticle();
+        void drawParticles();
+	void drawLines();
+        void updateLaser();
+        void updateParticles();
+public slots:
+	void compute();
+private:
+	InnerModelViewer *innerModelViewer;
+	InnerModel *innerModel;
+	OsgView *osgView;	
+	VectorLocalization2D *localization;
+	QTime t;
+	string curMapName;
+	vector2f initialLoc;
+	vector2f curLoc;
+        
+        //Point Cloud parameters
+        GVector::matrix3d<float> kinectToRobotTransform;
+        // KinectRawDepthCam kinectDepthCam;
+        KinectOpenNIDepthCam kinectDepthCam;
+        PlaneFilter::PlaneFilterParams filterParams;
+        PlaneFilter planeFilter;
+	//Generate filtered point cloud  
+	vector<vector3f> filteredPointCloud;
+	vector<vector3f> pointCloudNormals;
+	vector<vector3f> outlierCloud;
+	vector<vector2i> pixelLocs;
+	vector<PlanePolygon> planePolygons;
+	VectorLocalization2D::PointCloudParams pointCloudParams;
+
+
+	float curAngle;
+	float initialAngle;
+	float locUncertainty, angleUncertainty;
+	VectorLocalization2D::MotionModelParams motionParams;
+	VectorLocalization2D::LidarParams lidarParams;
+};
+
+#endif
+

b896d051bccdd8bdd65496dac1228c416b4b2b97
daniel1108 <dgallegos@alumnos.unex.es>
2015-06-25 12:21:46 +0200

    new world

--- experimental/CGR/src/specificworker.cpp
+++ experimental/CGR/src/specificworker.cpp
@@ -411,7 +411,7 @@ void SpecificWorker::drawLines()
                         oss << m.mapName << i;                        
 			InnerModelDraw::addPlane_notExisting(innerModelViewer,QString::fromStdString("LINEA_"+oss.str()),"floor",QVec::vec3(-(p0y+p1y)/2,0,(p0x+p1x)/2),
 							     QVec::vec3(-n(1),0,n(0)),"#00A0A0",QVec::vec3(width, 100, 100));	
-			printf("%f %f %f %f %f",-(p0y+p1y)/2,(p0x+p1x)/2),-n(1),n(0),width);
+			printf("%f %f %f %f %f",-(p0y+p1y)/2,(p0x+p1x)/2,-n(1),n(0),width);
 			i++;                        
 		}
 	}

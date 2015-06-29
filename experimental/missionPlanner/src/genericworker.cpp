f45cc4c3b097cad67405ab638ea3b2a0b1ccfce7
mhaut <juanmariohaut@unex.es>
2015-06-29 16:49:21 +0200

    audio

--- experimental/missionPlanner/src/genericworker.cpp
+++ experimental/missionPlanner/src/genericworker.cpp
@@ -21,7 +21,12 @@
 * \brief Default constructor
 */
 GenericWorker::GenericWorker(MapPrx& mprx) :
+#ifdef USE_QTGUI
+Ui_guiDlg()
+#else
 QObject()
+#endif
+
 {
 	trajectoryrobot2d_proxy = (*(TrajectoryRobot2DPrx*)mprx["TrajectoryRobot2DProxy"]);
 
@@ -29,6 +34,10 @@ QObject()
 
 	mutex = new QMutex(QMutex::Recursive);
 
+	#ifdef USE_QTGUI
+		setupUi(this);
+		show();
+	#endif
 		
 	Period = BASIC_PERIOD;
 	connect(&timer, SIGNAL(timeout()), this, SLOT(compute()));

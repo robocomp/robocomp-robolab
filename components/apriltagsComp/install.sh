b24600d8426e686c1a874ebf892fe3a0be90292c
pbustos <pbustos@unex.es>
2016-09-07 23:30:07 +0200

    remove  useless script

--- components/apriltagsComp/install.sh
+++
@@ -1,8 +0,0 @@
-#Script to install BifTK version of apriltags
-
-sudo apt-get install libv4l-dev libeigen3-dev
-git clone https://github.com/NifTK/apriltags.git
-cd apriltags
-cmake .
-make
-sudo make install

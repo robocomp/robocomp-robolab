524f982f58b9bd36e16e2dc8d4c03ff587d6fb02
pbustos <pbustos@unex.es>
2018-06-09 12:06:48 +0200

    new pyimu component for the new IMU device

---
+++ components/pyimu/src/specificworker.py
@@ -0,0 +1,60 @@
+#
+# Copyright (C) 2018 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys, os, traceback, time
+
+from PySide import QtGui, QtCore
+from genericworker import *
+
+# If RoboComp was compiled with Python bindings you can use InnerModel in Python
+# sys.path.append('/opt/robocomp/lib')
+# import librobocomp_qmat
+# import librobocomp_osgviewer
+# import librobocomp_innermodel
+
+class SpecificWorker(GenericWorker):
+	def __init__(self, proxy_map):
+		super(SpecificWorker, self).__init__(proxy_map)
+		self.timer.timeout.connect(self.compute)
+		self.Period = 500
+		self.timer.start(self.Period)
+
+	def setParams(self, params):
+		#try:
+		#	self.innermodel = InnerModel(params["InnerModelPath"])
+		#except:
+		#	traceback.print_exc()
+		#	print "Error reading config params"
+		return True
+
+	@QtCore.Slot()
+	def compute(self):
+		print 'SpecificWorker.compute...'
+		try:
+			imu = DataImu()
+
+			# aqui leeis del puerto serie y pasais los datos a imu
+
+			self.imupub_proxy.publish(imu)
+			print "sent data package"
+		except Ice.Exception, e:
+			traceback.print_exc()
+			print e
+		return True
+

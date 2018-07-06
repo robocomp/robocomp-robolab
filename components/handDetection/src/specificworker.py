506e8fa0e5b85e1551c172acc4874e468eeb44ba
Esteban Martinena Guerrero <orensbruli@gmail.com>
2018-07-06 17:22:31 +0200

    Added the HandDetection library Added initial code to the processImage method Some pep8 fixes Added *.idea to .gitignore

--- components/handDetection/src/specificworker.py
+++ components/handDetection/src/specificworker.py
@@ -21,6 +21,8 @@ import sys, os, traceback, time
 
 from PySide import QtGui, QtCore
 from genericworker import *
+from libs.HandDetection.HandDetection import HandDetector, Hand
+
 
 # If RoboComp was compiled with Python bindings you can use InnerModel in Python
 # sys.path.append('/opt/robocomp/lib')
@@ -29,48 +31,64 @@ from genericworker import *
 # import librobocomp_innermodel
 
 class SpecificWorker(GenericWorker):
-	def __init__(self, proxy_map):
-		super(SpecificWorker, self).__init__(proxy_map)
-		self.timer.timeout.connect(self.compute)
-		self.Period = 2000
-		self.timer.start(self.Period)
+    def __init__(self, proxy_map):
+        super(SpecificWorker, self).__init__(proxy_map)
+        self.timer.timeout.connect(self.compute)
+        self.Period = 2000
+        self.hand_detector = HandDetector()
+        self.timer.start(self.Period)
 
 
-	def setParams(self, params):
-		#try:
-		#	self.innermodel = InnerModel(params["InnerModelPath"])
-		#except:
-		#	traceback.print_exc()
-		#	print "Error reading config params"
-		return True
 
-	@QtCore.Slot()
-	def compute(self):
-		print 'SpecificWorker.compute...'
-		#computeCODE
-		#try:
-		#	self.differentialrobot_proxy.setSpeedBase(100, 0)
-		#except Ice.Exception, e:
-		#	traceback.print_exc()
-		#	print e
+    def setParams(self, params):
+        #try:
+        #	self.innermodel = InnerModel(params["InnerModelPath"])
+        #except:
+        #	traceback.print_exc()
+        #	print "Error reading config params"
+        return True
 
-		# The API of python-innermodel is not exactly the same as the C++ version
-		# self.innermodel.updateTransformValues("head_rot_tilt_pose", 0, 0, 0, 1.3, 0, 0)
-		# z = librobocomp_qmat.QVec(3,0)
-		# r = self.innermodel.transform("rgbd", z, "laser")
-		# r.printvector("d")
-		# print r[0], r[1], r[2]
+    @QtCore.Slot()
+    def compute(self):
+        print 'SpecificWorker.compute...'
+        #computeCODE
+        #try:
+        #	self.differentialrobot_proxy.setSpeedBase(100, 0)
+        #except Ice.Exception, e:
+        #	traceback.print_exc()
+        #	print e
 
-		return True
+        # The API of python-innermodel is not exactly the same as the C++ version
+        # self.innermodel.updateTransformValues("head_rot_tilt_pose", 0, 0, 0, 1.3, 0, 0)
+        # z = librobocomp_qmat.QVec(3,0)
+        # r = self.innermodel.transform("rgbd", z, "laser")
+        # r.printvector("d")
+        # print r[0], r[1], r[2]
+
+        return True
 
 
-	#
-	# processImage
-	#
-	def processImage(self, img):
-		ret = THands()
-		#
-		#implementCODE
-		#
-		return ret
+    #
+    # processImage
+    #
+    def processImage(self, img):
+        ret = THands()
+        if len(self.hand_detector.hands) < 1:
+            self.hand_detector.add_hand2()
+        else:
+            self.hand_detector.update_detection_and_tracking()
+            for detected_hand in self.hand_detector.hands:
+                new_hand = THand()
+                new_hand.id = detected_hand.id
+                new_hand.score = detected_hand.truth_value
+                new_hand.fingertips = detected_hand.fingertips
+                new_hand.intertips = detected_hand.intertips
+                new_hand.positions = detected_hand.position_history
+                new_hand.contour = detected_hand.contour
+                new_hand.centerMass = detected_hand.center_of_mass
+                new_hand.truthValue = detected_hand.truth_value
+                new_hand.detected = detected_hand.detected
+                new_hand.tracked = detected_hand.tracked
+                ret.append(new_hand)
+        return ret
 

aa56032514444d275961aa32fd7acbd78a98f66c
Esteban Martinena Guerrero <orensbruli@gmail.com>
2019-02-01 11:59:05 +0100

    Added code converters for the PointSeqStream

--- components/astraRGBD/src/DoubleBufferConverters.h
+++ components/astraRGBD/src/DoubleBufferConverters.h
@@ -1,3 +1,35 @@
+//
+// Created by robolab on 4/09/18.
+//
+
+#ifndef PROJECT_DOUBLEBUFFERCONVERTERS_H
+#define PROJECT_DOUBLEBUFFERCONVERTERS_H
+
+#include <qdebug.h>
+
+
+//TODO: This implementantions should go it's own file
+class ByteSeqConverter : public Converter<astra::ColorFrame, RoboCompRGBD::imgType>
+{
+public:
+    bool ItoO(const astra::ColorFrame &iTypeData, RoboCompRGBD::imgType &oTypeData)
+    {
+        if (iTypeData.is_valid())
+        {
+
+            //            this->resize(d.width() * d.height()*data_size);
+            memcpy(&oTypeData[0], iTypeData.data(), iTypeData.width()*iTypeData.height()*3);
+            //            std::copy(std::begin(d.data()), std::end(d.data()), std::begin(writeBuffer));
+            return true;
+        }
+        return false;
+    }
+
+    bool OtoI(const RoboCompRGBD::imgType &oTypeData, astra::ColorFrame &iTypeData)
+    {
+        return false;
+    }
+};
 
 class PointSeqConverter : public Converter<astra::PointFrame, RoboCompRGBD::PointSeq>
 {
@@ -6,6 +38,7 @@ public:
     {
         if (iTypeData.is_valid())
         {
+
             //            this->resize(d.width() * d.height()*data_size);
 //            std::cout<<"iTypeData length "<<iTypeData.length()<<endl;
 //            memcpy(&oTypeData[0], iTypeData.data(), iTypeData.length());
@@ -46,7 +79,7 @@ public:
 //            std::cout<<"iTypeData length "<<iTypeData.length()<<endl;
             memcpy(&oTypeData[0], iTypeData.data(), iTypeData.length()*3*4);
             auto point = iTypeData.data()[10];
-			std::cout<<"Point ("<<point.x<<", "<<point.y<<", "<<point.z<<")"<<std::endl;
+//			std::cout<<"Point ("<<point.x<<", "<<point.y<<", "<<point.z<<")"<<std::endl;
 //            int non0 = 0;
 //			for(uint i = 0; i< iTypeData.length(); i++)
 //			{
@@ -71,6 +104,65 @@ public:
     }
 };
 
+class ColorSeqConverter : public Converter<astra::ColorFrame, RoboCompRGBD::ColorSeq>
+{
+public:
+    bool ItoO(const astra::ColorFrame &iTypeData, RoboCompRGBD::ColorSeq &oTypeData)
+    {
+        if (iTypeData.is_valid())
+        {
+
+            //            this->resize(d.width() * d.height()*data_size);
+            memcpy(&oTypeData[0], iTypeData.data(), iTypeData.width()*iTypeData.height()*3);
+            //            std::copy(std::begin(d.data()), std::end(d.data()), std::begin(writeBuffer));
+            return true;
+        }
+        return false;
+    }
+
+    bool OtoI(const RoboCompRGBD::ColorSeq &oTypeData, astra::ColorFrame &iTypeData)
+    {
+        return false;
+    }
+};
+//class ByteSeqConverter : public Converter<astra::ColorFrame, RoboCompRGBD::imgType>
+//{
+//public:
+//    bool ItoO(const astra::ColorFrame &iTypeData, RoboCompRGBD::imgType &oTypeData)
+//    {
+//        if (iTypeData.is_valid())
+//        {
+//
+//            //            this->resize(d.width() * d.height()*data_size);
+//            memcpy(&oTypeData[0], iTypeData.data(), iTypeData.width()*iTypeData.height()*3);
+//            //            std::copy(std::begin(d.data()), std::end(d.data()), std::begin(writeBuffer));
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    bool OtoI(const RoboCompRGBD::imgType &oTypeData, astra::ColorFrame &iTypeData)
+//    {
+//        return false;
+//    }
+//};
+
+class FloatSeqConverter : public Converter<astra::DepthFrame, RoboCompRGBD::DepthSeq>
+{
+public:
+    bool ItoO(const astra::DepthFrame &iTypeData, RoboCompRGBD::DepthSeq &oTypeData)
+    {
+        if (iTypeData.is_valid())
+        {
+            std::copy(&iTypeData.data()[0], &iTypeData.data()[0]+(iTypeData.width()*iTypeData.height()), std::begin(oTypeData));
+            return true;
+        }
+        return false;
+    }
+
+    bool OtoI(const RoboCompRGBD::DepthSeq &oTypeData, astra::DepthFrame &iTypeData)
+    {
+        return false;
     }
 };
 

9c2a46c34cb20f0f39e53804ce83a211e6670e35
bartekxk <bartekkocot@op.pl>
2019-02-13 03:59:52 +0100

    Second Joystick
    Adding a second joystick for robot rotation

--- components/joystickSimulatorController/src/joystickSimulatorController.py
+++ components/joystickSimulatorController/src/joystickSimulatorController.py
@@ -24,47 +24,72 @@ from PySide import *
 class MyGui(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
-        self.setGeometry(50,50,500,500)
+        self.setGeometry(50,50,1000,500)
         self.setWindowTitle("Joystick Simulator Controller")
 	self.setStyleSheet("QMainWindow {background: 'white';}");
         self.show()
 	self.addJoystickImage()
     def addJoystickImage(self):
-	self.label = QtGui.QLabel(self)
+	self.leftJoystick = QtGui.QLabel(self)
+	self.rightJoystick = QtGui.QLabel(self)
 	pixmap = QtGui.QPixmap('src/img/joystick.png')
-	self.label.setObjectName("JoyStick")
-	self.label.setPixmap(pixmap)
-	self.label.resize(50,50)
-	self.label.move(225,225)	
-	self.label.show()
-    def setPosition(self,x,y):
-	self.label.move(x,y)
-    def comeBack(self):
-	self.label.move(225,225)
+	self.leftJoystick.setObjectName("leftJoystick")
+	self.rightJoystick.setObjectName("rightJoystick")
+	self.leftJoystick.setPixmap(pixmap)
+	self.rightJoystick.setPixmap(pixmap)
+	self.leftJoystick.resize(50,50)
+	self.rightJoystick.resize(50,50)
+	self.leftJoystick.move(225,225)
+	self.rightJoystick.move(725,225)
+	self.leftJoystick.show()
+	self.rightJoystick.show()
+    def setPosition(self,joystickName):
+	if joystickName == "leftJoystick":
+	    self.leftJoystick.move(leftJoystickX,leftJoystickY)
+	else:
+	    self.rightJoystick.move(rightJoystickX,rightJoystickY)
+    def comeBack(self,joystickName):
+	if joystickName == "leftJoystick":
+	    self.leftJoystick.move(225,225)
+	else:
+	    self.rightJoystick.move(725,225)
 
 class MouseDetector(QtCore.QObject):
     def eventFilter(self, obj, event):
-	global mousePress, mygui,x,y
-        if obj.objectName() == "JoyStick":
+	global mousePress, mygui,leftJoystickX,leftJoystickY,rightJoystickX,rightJoystickY
+        if obj.objectName() == "leftJoystick" or obj.objectName() == "rightJoystick":
 	    if event.type() == QtCore.QEvent.Type.MouseButtonPress:
 		mousePress = True
 	    if event.type() == QtCore.QEvent.Type.MouseButtonRelease:
 	    	mousePress = False
-		mygui.comeBack()
+		mygui.comeBack(obj.objectName())
 	    if event.type() == QtCore.QEvent.Type.MouseMove and mousePress == True:
-		print event.x(),event.y()
-		if self.distance(x+event.x()-25,y+event.y()-25) < 100:
-			x=x+event.x()-25
-			y=y+event.y()-25
-			mygui.setPosition(x,y)
+		if self.distance(event.x(),event.y(),obj.objectName()) < 100:
+		    if obj.objectName() == "leftJoystick":
+			leftJoystickX=leftJoystickX+event.x()-25
+			leftJoystickY=leftJoystickY+event.y()-25
+		    else:
+			rightJoystickX=rightJoystickX+event.x()-25
+			rightJoystickY=rightJoystickY+event.y()-25
+		    mygui.setPosition(obj.objectName())
         return super(MouseDetector, self).eventFilter(obj, event)
-    def distance(self,x,y):
-	result = (225-x)*(225-x)+(225-y)*(225-y)
+    def distance(self,x,y,joystickName):
+	if joystickName == "leftJoystick":
+	    x=leftJoystickX+x-25
+	    y=leftJoystickY+y-25
+	    result = (225-x)*(225-x)+(225-y)*(225-y)
+	else:
+	    x=rightJoystickX+x-25
+	    y=rightJoystickY+y-25
+	    result = (725-x)*(725-x)+(225-y)*(225-y)
 	result = math.sqrt(result)
 	return result
+	
 mousePress = False
-x=225
-y=225
+leftJoystickX=225
+leftJoystickY=225
+rightJoystickX=725
+rightJoystickY=225
 app = QtGui.QApplication(sys.argv)
 mouseFilter = MouseDetector()
 app.installEventFilter(mouseFilter)

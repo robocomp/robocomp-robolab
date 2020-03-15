78225fb8927ee6382276e83dbda4738af9ed8d98
nagasai-iitr <40641467+nagasai-iitr@users.noreply.github.com>
2020-03-16 03:04:40 +0530

    New control algorithm
    The previous commit contains the algorithm for controlling the robots' movements in terms of speed and direction but here I have updated the file so as to control the robot in such a way one can control a car in a normal game. 
    Even though we here are restricting the complete use of 
    ```
    differentialrobot_proxy.setSpeedBase(adv, rot)
    ```
    but since ultimately the use of this component is in real life in which people's comfort of using the gear or component is the first priority of making the component 
    
    NOTE : Here the values of speed are discrete but we can omit the code evenmore by adding one more button for changing the discrete values

--- components/hardware/external_control/keyboardrobotcontroller/src/specificworker.py
+++ components/hardware/external_control/keyboardrobotcontroller/src/specificworker.py
@@ -17,7 +17,7 @@
 #    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import sys, os, Ice, traceback, curses
+import sys, os, Ice, traceback, curses, pygame
 from PySide2 import *
 from genericworker import *
 
@@ -52,14 +52,16 @@ Ice.loadSlice(preStr+"Laser.ice")
 from RoboCompLaser import *
 
 class SpecificWorker(GenericWorker):
-	adv = 0
-	rot = 0
+	adv1 = 0
+	rot1 = 0
 	def __init__(self, proxy_map):
 		super(SpecificWorker, self).__init__(proxy_map)
 		self.timer.timeout.connect(self.compute)
 		self.Period = 1
+		pygame.init()
+		pygame.display.set_mode((1,1), pygame.NOFRAME)
 		self.timer.start(self.Period)
-		screen.addstr(0,0,'Connected to robot. Use arrows to control speed, space bar to stop ans ''q'' to exit')
+		screen.addstr(0,0,'Connected to robot. Use arrows (or) ASWD to control direction,Shift to speed, space bar to stop ans ''q'' to exit')
 		#tt1=input("max :")
 		#tt2=input("min :")
 
@@ -72,46 +74,87 @@ class SpecificWorker(GenericWorker):
 	@QtCore.Slot()
 	def compute(self):
 		try:
-			key = screen.getch()
-            
-			if key == curses.KEY_UP:
-				if self.adv  > self.tt1 :
-					self.adv=self.adv
-				else :
-					self.adv = self.adv + 20;
-				screen.addstr(5, 0, 'up: '+ '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == curses.KEY_DOWN:
-				if self.adv < -1*self.tt1 :
-					self.adv=self.adv
-				else :
-					self.adv = self.adv - 20;
-				screen.addstr(5, 0, 'down: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == curses.KEY_LEFT:
-				if self.rot < -1*self.tt2 :
-					self.rot=self.rot
-				else :
-					self.rot = self.rot - 0.1;
-				screen.addstr(5, 0, 'left: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == curses.KEY_RIGHT:
-				if self.rot > self.tt2 :
-					self.rot=self.rot
-				else :
-					self.rot = self.rot + 0.1;
-				screen.addstr(5, 0, 'right: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == ord(' '):
-				self.rot = 0;
-				self.adv = 0;
-				screen.addstr(5, 0, 'stop: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == ord('q'):
-				curses.endwin()
-				sys.exit() 
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					pygame.quit()
+					sys.exit()
+				
+				elif event.type == KEYDOWN:
+					if event.key == pygame.K_UP or event.key == pygame.K_w:
+						if self.adv1 < self.tt1:
+							self.adv1 += 300
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+						screen.addstr(5, 0, 'UP: '+ '%.2f' % self.adv1 )
+						screen.refresh()
+					if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+						if self.adv1 > -1*self.tt1:
+							self.adv1 -= 300
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+						screen.addstr(5, 0, 'DOWN: '+ '%.2f' % self.adv1 )
+						screen.refresh()
+					if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+						if self.adv1 >= 0:
+							if self.rot1 < self.tt2:
+								self.rot1 += 0.25
+						else :
+							if self.rot1 > -1*self.tt2:
+								self.rot1 -=0.25
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+						screen.addstr(6, 0, 'RIGHT: '+ '%.2f' % self.rot1)
+						screen.refresh()
+					if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+						if self.adv1 >= 0:
+							if self.rot1 > -1*self.tt2:
+								self.rot1 -=0.25
+						else :
+							if self.rot1 < self.tt2:
+								self.rot1 += 0.25
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+						screen.addstr(6, 0, 'LEFT: '+ '%.2f' % self.rot1)
+						screen.refresh()
+					if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+						self.adv1 *=2
+						self.rot1 *=1.7
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+						screen.addstr(5, 0, 'UP: '+ '%.2f' % self.adv1 )
+						screen.addstr(6, 0, 'RIGHT: '+ '%.2f' % self.rot1 )
+						screen.refresh()
+					if pygame.key.get_pressed()[K_SPACE]:
+						self.rot1 = 0
+						self.adv1 = 0
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+						screen.addstr(5, 0, 'STOP: '+ '%.2f' % self.adv1 )
+						screen.addstr(6, 0, 'STOP: '+ '%.2f' % self.rot1 )
+						screen.refresh()
+					if event.key == pygame.K_q:
+						pygame.quit()
+						sys.exit()
+				elif event.type == KEYUP:
+					if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+						#if self.adv1 < self.tt1:
+						self.adv1 =0
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+					if event.key == pygame.K_UP or event.key == pygame.K_w:
+						#if self.adv1 > -1*self.tt1:
+						self.adv1 =0
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+					if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+						#if self.rot1 < self.tt2:
+						self.rot1 =0
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+					if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+						#if self.rot1 > -1*self.tt2:
+						self.rot1 =0
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+					if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+						self.adv1 /=2
+						self.rot1 /=1.7
+						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
+						
 		except Ice.Exception as e:
 			curses.endwin()
+			pygame.quit()
+			sys.exit()
 			traceback.print_exc()
 			print (e)
 		return True

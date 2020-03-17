508e7feabbf2238074bca0cbd3dcd6140d185217
nagasai-iitr <40641467+nagasai-iitr@users.noreply.github.com>
2020-03-17 11:17:08 +0530

    updated to our needs
    Included the required needs for the algorithm so that the new algorithm works if there is an availability of pygame

--- components/hardware/external_control/keyboardrobotcontroller/src/specificworker.py
+++ components/hardware/external_control/keyboardrobotcontroller/src/specificworker.py
@@ -17,7 +17,7 @@
 #    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import sys, os, Ice, traceback, curses, pygame
+import sys, os, Ice, traceback, curses
 from PySide2 import *
 from genericworker import *
 
@@ -56,12 +56,20 @@ class SpecificWorker(GenericWorker):
 	rot1 = 0
 	def __init__(self, proxy_map):
 		super(SpecificWorker, self).__init__(proxy_map)
-		self.timer.timeout.connect(self.compute)
-		self.Period = 1
-		pygame.init()
-		pygame.display.set_mode((1,1), pygame.NOFRAME)
-		self.timer.start(self.Period)
-		screen.addstr(0,0,'Connected to robot. Use arrows (or) ASWD to control direction,Shift to speed, space bar to stop ans ''q'' to exit')
+		try:
+			import pygame
+			
+			self.timer.timeout.connect(self.compute1)
+			self.Period = 1
+			pygame.init()
+			pygame.display.set_mode((1,1), pygame.NOFRAME)
+			self.timer.start(self.Period)
+			screen.addstr(0,0,'Connected to robot. Use arrows (or) ASWD to control direction,Shift to speed, space bar to stop ans ''q'' to exit')
+		except ImportError as e:
+			self.timer.timeout.connect(self.compute2)
+			self.Period = 1
+			self.timer.start(self.Period)
+			screen.addstr(0,0,'Connected to robot. Use arrows to control speed, space bar to stop ans ''q'' to exit')
 		#tt1=input("max :")
 		#tt2=input("min :")
 
@@ -72,7 +80,7 @@ class SpecificWorker(GenericWorker):
 	tt2=2
 
 	@QtCore.Slot()
-	def compute(self):
+	def compute1(self):
 		try:
 			for event in pygame.event.get():
 				if event.type == QUIT:
@@ -151,6 +159,54 @@ class SpecificWorker(GenericWorker):
 						self.rot1 /=1.7
 						self.differentialrobot_proxy.setSpeedBase(self.adv1, self.rot1)
 						
+		except Ice.Exception as e:
+			curses.endwin()
+			pygame.quit()
+			sys.exit()
+			traceback.print_exc()
+			print (e)
+		return True
+	
+	def compute2(self):
+		try:
+			key = screen.getch()
+
+			if key == curses.KEY_UP:
+				if self.adv  > self.tt1 :
+					self.adv=self.adv
+				else :
+					self.adv = self.adv + 20;
+				screen.addstr(5, 0, 'up: '+ '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
+			elif key == curses.KEY_DOWN:
+				if self.adv < -1*self.tt1 :
+					self.adv=self.adv
+				else :
+					self.adv = self.adv - 20;
+				screen.addstr(5, 0, 'down: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
+				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
+			elif key == curses.KEY_LEFT:
+				if self.rot < -1*self.tt2 :
+					self.rot=self.rot
+				else :
+					self.rot = self.rot - 0.1;
+				screen.addstr(5, 0, 'left: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
+				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
+			elif key == curses.KEY_RIGHT:
+				if self.rot > self.tt2 :
+					self.rot=self.rot
+				else :
+					self.rot = self.rot + 0.1;
+				screen.addstr(5, 0, 'right: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
+				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
+			elif key == ord(' '):
+				self.rot = 0;
+				self.adv = 0;
+				screen.addstr(5, 0, 'stop: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
+				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
+			elif key == ord('q'):
+				curses.endwin()
+				sys.exit() 
 		except Ice.Exception as e:
 			curses.endwin()
 			pygame.quit()

8d3fa73332312127f48a1c8da78999f8facf9bf0
nagasai-iitr <40641467+nagasai-iitr@users.noreply.github.com>
2020-03-19 18:45:37 +0530

    New control algorithm for keyboardrobotcontroler (#50)
    * New control algorithm 
    
    The previous commit contains the algorithm for controlling the robots' movements in terms of speed and direction but here I have updated the file so as to control the robot in such a way one can control a car in a normal game. 
    Even though we here are restricting the complete use of 
    ```
    differentialrobot_proxy.setSpeedBase(adv, rot)
    ```
    but since ultimately the use of this component is in real life in which people's comfort of using the gear or component is the first priority of making the component 
    
    NOTE : Here the values of speed are discrete but we can omit the code evenmore by adding one more button for changing the discrete values
    
    * updated to our needs
    
    Included the required needs for the algorithm so that the new algorithm works if there is an availability of pygame
    
    * Updated to our needs
    
    Updated the program in such a way which suffices our needs that if there is any availability of any pygame module the new algorithm works and if not remains with the previous one
    
    * added flag 
    
    added flag for import of pygame, kept the compute function unique itself and by using the flag included the new algorithm in the compute method
    
    * Changes to the methods, code unification, pep8 and better exception handling.
    
    * Minor fix of spaces.
    
    * Fixing the ncurses compute method.
    
    * Fixes to the pygame_compute method of keyboardrobotcontroller
    
    Co-authored-by: Esteban Martinena Guerrero <orensbruli@gmail.com>

--- components/hardware/external_control/keyboardrobotcontroller/src/specificworker.py
+++ components/hardware/external_control/keyboardrobotcontroller/src/specificworker.py
@@ -20,29 +20,35 @@
 import sys, os, Ice, traceback, curses
 from PySide2 import *
 from genericworker import *
+try:
+    import pygame
+    from pygame.locals import *
+    pygame_flag = True  # Flag for presence of pygame
+except ImportError as e:
+    pygame_flag = False
 
 # get the curses screen window
 screen = curses.initscr()
- 
+
 # turn off input echoing
 curses.noecho()
- 
+
 # respond to keys immediately (don't wait for enter)
 curses.cbreak()
- 
+
 # map arrow keys to special values
 screen.keypad(True)
  
 ROBOCOMP = ''
 
 try:
-	ROBOCOMP = os.environ['ROBOCOMP']
+    ROBOCOMP = os.environ['ROBOCOMP']
 except:
-	print ('$ROBOCOMP environment variable not set, using the default value /opt/robocomp')
-	ROBOCOMP = '/opt/robocomp'
+    print ('$ROBOCOMP environment variable not set, using the default value /opt/robocomp')
+    ROBOCOMP = '/opt/robocomp'
 if len(ROBOCOMP)<1:
-	print ('genericworker.py: ROBOCOMP environment variable not set! Exiting.')
-	sys.exit()
+    print ('genericworker.py: ROBOCOMP environment variable not set! Exiting.')
+    sys.exit()
 
 
 preStr = "-I"+ROBOCOMP+"/interfaces/ --all "+ROBOCOMP+"/interfaces/"
@@ -52,71 +58,180 @@ Ice.loadSlice(preStr+"Laser.ice")
 from RoboCompLaser import *
 
 class SpecificWorker(GenericWorker):
-	adv = 0
-	rot = 0
-	def __init__(self, proxy_map):
-		super(SpecificWorker, self).__init__(proxy_map)
-		self.timer.timeout.connect(self.compute)
-		self.Period = 1
-		self.timer.start(self.Period)
-		screen.addstr(0,0,'Connected to robot. Use arrows to control speed, space bar to stop ans ''q'' to exit')
-		#tt1=input("max :")
-		#tt2=input("min :")
 
-	def setParams(self, params):
-		return True
+    def __init__(self, proxy_map):
+        super(SpecificWorker, self).__init__(proxy_map)
+        self.timer.timeout.connect(self.compute)
+        self.Period = 1
+        self.timer.start(self.Period)
+        self.adv = 0
+        self.rot = 0
+        self.tt1 = 2000
+        self.tt2 = 2
+        if pygame_flag:
+            pygame.init()
+            pygame.display.set_mode((1,1), pygame.NOFRAME)
+            screen.addstr(0,0,'Connected to robot. Use arrows (or) ASWD to control direction, Shift to speed up, space bar to stop ans ''q'' to exit')
+            self.adv_command = 'STOP'
+            self.rot_command = 'STOP'
+        else:
+            screen.addstr(0,0,'Connected to robot. Use arrows to control speed, space bar to stop ans ''q'' to exit')
+        screen.refresh()
 
-	tt1=2000
-	tt2=2
+        #tt1=input("max :")
+        #tt2=input("min :")
 
-	@QtCore.Slot()
-	def compute(self):
-		try:
-			key = screen.getch()
-            
-			if key == curses.KEY_UP:
-				if self.adv  > self.tt1 :
-					self.adv=self.adv
-				else :
-					self.adv = self.adv + 20;
-				screen.addstr(5, 0, 'up: '+ '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == curses.KEY_DOWN:
-				if self.adv < -1*self.tt1 :
-					self.adv=self.adv
-				else :
-					self.adv = self.adv - 20;
-				screen.addstr(5, 0, 'down: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == curses.KEY_LEFT:
-				if self.rot < -1*self.tt2 :
-					self.rot=self.rot
-				else :
-					self.rot = self.rot - 0.1;
-				screen.addstr(5, 0, 'left: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == curses.KEY_RIGHT:
-				if self.rot > self.tt2 :
-					self.rot=self.rot
-				else :
-					self.rot = self.rot + 0.1;
-				screen.addstr(5, 0, 'right: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == ord(' '):
-				self.rot = 0;
-				self.adv = 0;
-				screen.addstr(5, 0, 'stop: '+ '%.2f' % self.adv+ ' : ' + '%.2f' % self.rot)
-				self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
-			elif key == ord('q'):
-				curses.endwin()
-				sys.exit() 
-		except Ice.Exception as e:
-			curses.endwin()
-			traceback.print_exc()
-			print (e)
-		return True
+    def __del__(self):
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
+
+    def setParams(self, params):
+        return True
 
 
+    @QtCore.Slot()
+    def compute(self):
+        if pygame_flag:
+            result = self.pygame_compute()
+        else :
+            result = self.ncurses_compute()
+
+        if result is True:
+            try:
+                self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
+            except Ice.Exception as e:
+                curses.endwin()
+                traceback.print_exc()
+                if pygame_flag:
+                    pygame.quit()
+                    sys.exit()
+                print(e)
+        return True
+
+    def pygame_compute(self):
+        # control algorithm for presence of pygame
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit(0)
+
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    if self.adv < self.tt1:
+                        self.adv += 300
+                    self.adv_command = 'UP'
+
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    if self.adv > -1 * self.tt1:
+                        self.adv -= 300
+                    self.adv_command = 'DOWN'
+
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    if self.adv >= 0:
+                        if self.rot < self.tt2:
+                            self.rot += 0.25
+                    else:
+                        if self.rot > -1 * self.tt2:
+                            self.rot -= 0.25
+                    self.rot_command = "RIGHT"
+
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    if self.adv >= 0:
+                        if self.rot > -1 * self.tt2:
+                            self.rot -= 0.25
+                    else:
+                        if self.rot < self.tt2:
+                            self.rot += 0.25
+                    self.rot_command = "LEFT"
+
+                if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+                    self.adv *= 2
+                    self.rot *= 1.7
+
+                if pygame.key.get_pressed()[K_SPACE]:
+                    self.rot = 0
+                    self.adv = 0
+                    self.adv_command = 'STOP'
+                    self.rot_command = 'STOP'
+
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit(0)
+                screen.addstr(5, 0, self.adv_command+': ' + '%.2f' % self.adv)
+                screen.clrtoeol()
+                screen.addstr(6, 0, self.rot_command+': ' + '%.2f' % self.rot)
+                screen.clrtoeol()
+                screen.refresh()
+                return True
+            if event.type == KEYUP:
+                if event.key == pygame.K_DOWN or event.key == pygame.K_UP or event.key == pygame.K_s or event.key == pygame.K_w:
+                    self.adv = 0
+                    self.adv_command = 'STOP'
+                if event.key == pygame.K_LEFT or pygame.K_RIGHT or event.key == pygame.K_d or event.key == pygame.K_a:
+                    # if self.rot < self.tt2:
+                    self.rot = 0
+                    self.rot_command = 'STOP'
+                if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+                    self.adv /= 2
+                    self.rot /= 1.7
+                screen.addstr(5, 0, self.adv_command + ': ' + '%.2f' % self.adv)
+                screen.clrtoeol()
+                screen.addstr(6, 0, self.rot_command + ': ' + '%.2f' % self.rot)
+                screen.clrtoeol()
+                screen.refresh()
+                return True
+            else:
+                return False
+
+
+
+    def ncurses_compute(self):
+        # control algorithm for absence of pygame
+        key = screen.getch()
+        if key == curses.KEY_UP:
+            if self.adv > self.tt1:
+                self.adv = self.adv
+            else:
+                self.adv = self.adv + 20
+            screen.addstr(5, 0, 'up: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            return True
+
+        elif key == curses.KEY_DOWN:
+            if self.adv < -1 * self.tt1:
+                self.adv = self.adv
+            else:
+                self.adv = self.adv - 20
+            screen.addstr(5, 0, 'down: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            return True
+
+        elif key == curses.KEY_LEFT:
+            if self.rot < -1 * self.tt2:
+                self.rot = self.rot
+            else:
+                self.rot = self.rot - 0.1
+            screen.addstr(5, 0, 'left: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            return True
+
+        elif key == curses.KEY_RIGHT:
+            if self.rot > self.tt2:
+                self.rot = self.rot
+            else:
+                self.rot = self.rot + 0.1
+            screen.addstr(5, 0, 'right: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            return True
+
+        elif key == ord(' '):
+            self.rot = 0
+            self.adv = 0
+            screen.addstr(5, 0, 'stop: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            return True
+
+        elif key == ord('q'):
+            curses.endwin()
+            sys.exit(0)
+
+        return 0
 
 
 

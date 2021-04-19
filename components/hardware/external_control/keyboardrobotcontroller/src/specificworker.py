c6137e24d75cadec974d02ed32b4853b4c2777d6
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-04-19 19:09:41 +0200

    [components] Updates and fix to keyboarcontroller component

--- components/hardware/external_control/keyboardrobotcontroller/src/specificworker.py
+++ components/hardware/external_control/keyboardrobotcontroller/src/specificworker.py
@@ -20,79 +20,92 @@
 import sys, os, Ice, traceback, curses
 from PySide2 import *
 from genericworker import *
+
 try:
     import pygame
     from pygame.locals import *
     pygame_flag = True  # Flag for presence of pygame
+    print("PyGame imported...")
 except ImportError as e:
     pygame_flag = False
+    print("PyGame not installed. Using ncurses ...")
 
-# get the curses screen window
-screen = curses.initscr()
 
-# turn off input echoing
-curses.noecho()
-
-# respond to keys immediately (don't wait for enter)
-curses.cbreak()
-
-# map arrow keys to special values
-screen.keypad(True)
- 
 
 class SpecificWorker(GenericWorker):
 
     def __init__(self, proxy_map, startup_check=False):
         super(SpecificWorker, self).__init__(proxy_map)
-        self.timer.timeout.connect(self.compute)
+        # get the curses self.screen window
+        self.screen = curses.initscr()
+        # turn off input echoing
+        curses.noecho()
+        # respond to keys immediately (don't wait for enter)
+        curses.cbreak()
+        # map arrow keys to special values
+        self.screen.keypad(True)
         self.Period = 1
         if startup_check:
             self.startup_check()
         else:
-            self.timer.start(self.Period)
+
             self.adv = 0
             self.rot = 0
             self.tt1 = 2000
             self.tt2 = 2
             if pygame_flag:
                 pygame.init()
-                pygame.display.set_mode((1,1), pygame.NOFRAME)
-                screen.addstr(0,0,'Connected to robot. Use arrows (or) ASWD to control direction, Shift to speed up, space bar to stop ans ''q'' to exit')
+                pygame.display.set_mode((1, 1), pygame.NOFRAME)
+                self.screen.addstr(0, 0,
+                              'Connected to robot. Use arrows (or) ASWD to control direction, Shift to speed up, space bar to stop and ''q'' to exit')
                 self.adv_command = 'STOP'
                 self.rot_command = 'STOP'
             else:
-                screen.addstr(0,0,'Connected to robot. Use arrows to control speed, space bar to stop ans ''q'' to exit')
-            screen.refresh()
+                self.screen.addstr(0, 0,
+                              'Connected to robot. Use arrows to control speed, space bar to stop and ''q'' to exit')
+            self.screen.refresh()
+            self.timer.timeout.connect(self.compute)
+            self.timer.start(self.Period)
 
-            #tt1=input("max :")
-            #tt2=input("min :")
+            # tt1=input("max :")
+            # tt2=input("min :")
 
     def __del__(self):
-        curses.nocbreak()
-        curses.echo()
-        curses.endwin()
+        pass
+        # self.end_run()
+
+    def end_run(self):
+        print("Ending...")
+        try:
+            if pygame_flag:
+                pygame.quit()
+            if curses is not None:
+                self.screen.clear()
+                curses.endwin()
+                curses.echo()
+                curses.nocbreak()
+            self.timer.stop()
+            QtCore.QCoreApplication.instance().quit()
+        except TypeError:
+            print("end_run: Problems ending process")
 
     def setParams(self, params):
         return True
-
 
     @QtCore.Slot()
     def compute(self):
         if pygame_flag:
             result = self.pygame_compute()
-        else :
+        else:
             result = self.ncurses_compute()
-
+        self.screen.refresh()
         if result is True:
             try:
                 self.differentialrobot_proxy.setSpeedBase(self.adv, self.rot)
             except Ice.Exception as e:
-                curses.endwin()
                 traceback.print_exc()
-                if pygame_flag:
-                    pygame.quit()
-                    sys.exit()
                 print(e)
+                self.end_run()
         return True
 
     def startup_check(self):
@@ -100,12 +113,16 @@ class SpecificWorker(GenericWorker):
 
     def pygame_compute(self):
         # control algorithm for presence of pygame
+
         for event in pygame.event.get():
+            self.screen.move(2, 0)
+            self.screen.clrtoeol()
+            self.screen.addstr(2, 0, f'pygame compute {pygame.event.event_name(event.type)}')
             if event.type == QUIT:
-                pygame.quit()
-                sys.exit(0)
+                self.end_run()
 
             if event.type == KEYDOWN:
+                self.screen.addstr(3, 0, f'Key: {pygame.key.name(event.key)}')
                 if event.key == pygame.K_UP or event.key == pygame.K_w:
                     if self.adv < self.tt1:
                         self.adv += 300
@@ -144,16 +161,17 @@ class SpecificWorker(GenericWorker):
                     self.adv_command = 'STOP'
                     self.rot_command = 'STOP'
 
-                if event.key == pygame.K_q:
-                    pygame.quit()
-                    sys.exit(0)
-                screen.addstr(5, 0, self.adv_command+': ' + '%.2f' % self.adv)
-                screen.clrtoeol()
-                screen.addstr(6, 0, self.rot_command+': ' + '%.2f' % self.rot)
-                screen.clrtoeol()
-                screen.refresh()
+                if event.key == pygame.K_q or event.key == ord('q'):
+                    self.end_run()
+                self.screen.addstr(5, 0, self.adv_command + ': ' + '%.2f' % self.adv)
+                self.screen.clrtoeol()
+                self.screen.addstr(6, 0, self.rot_command + ': ' + '%.2f' % self.rot)
+                self.screen.clrtoeol()
+                self.screen.refresh()
                 return True
             if event.type == KEYUP:
+                self.screen.move(3, 0)
+                self.screen.clrtoeol()
                 if event.key == pygame.K_DOWN or event.key == pygame.K_UP or event.key == pygame.K_s or event.key == pygame.K_w:
                     self.adv = 0
                     self.adv_command = 'STOP'
@@ -164,64 +182,61 @@ class SpecificWorker(GenericWorker):
                 if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
                     self.adv /= 2
                     self.rot /= 1.7
-                screen.addstr(5, 0, self.adv_command + ': ' + '%.2f' % self.adv)
-                screen.clrtoeol()
-                screen.addstr(6, 0, self.rot_command + ': ' + '%.2f' % self.rot)
-                screen.clrtoeol()
-                screen.refresh()
+                self.screen.addstr(5, 0, self.adv_command + ': ' + '%.2f' % self.adv)
+                self.screen.clrtoeol()
+                self.screen.addstr(6, 0, self.rot_command + ': ' + '%.2f' % self.rot)
+                self.screen.clrtoeol()
+                self.screen.refresh()
                 return True
             else:
                 return False
 
-
-
     def ncurses_compute(self):
         # control algorithm for absence of pygame
-        key = screen.getch()
-        if key == curses.KEY_UP:
+        self.screen.addstr(2, 0, 'ncurses compute')
+        self.screen.refresh()
+        key = self.screen.getch()
+        if key == curses.KEY_UP or key == ord('w'):
             if self.adv > self.tt1:
                 self.adv = self.adv
             else:
                 self.adv = self.adv + 20
-            screen.addstr(5, 0, 'up: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            self.screen.addstr(5, 0, 'up: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
             return True
 
-        elif key == curses.KEY_DOWN:
+        elif key == curses.KEY_DOWN or key == ord('s'):
             if self.adv < -1 * self.tt1:
                 self.adv = self.adv
             else:
                 self.adv = self.adv - 20
-            screen.addstr(5, 0, 'down: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            self.screen.addstr(5, 0, 'down: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
             return True
 
-        elif key == curses.KEY_LEFT:
+        elif key == curses.KEY_LEFT or key == ord('a'):
             if self.rot < -1 * self.tt2:
                 self.rot = self.rot
             else:
                 self.rot = self.rot - 0.1
-            screen.addstr(5, 0, 'left: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            self.screen.addstr(5, 0, 'left: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
             return True
 
-        elif key == curses.KEY_RIGHT:
+        elif key == curses.KEY_RIGHT or key == ord('d'):
             if self.rot > self.tt2:
                 self.rot = self.rot
             else:
                 self.rot = self.rot + 0.1
-            screen.addstr(5, 0, 'right: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            self.screen.addstr(5, 0, 'right: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
             return True
 
         elif key == ord(' '):
             self.rot = 0
             self.adv = 0
-            screen.addstr(5, 0, 'stop: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
+            self.screen.addstr(5, 0, 'stop: ' + '%.2f' % self.adv + ' : ' + '%.2f' % self.rot)
             return True
 
         elif key == ord('q'):
-            curses.endwin()
-            sys.exit(0)
-
+            self.end_run()
         return 0
-
 
     ######################
     # From the RoboCompDifferentialRobot you can call this methods:

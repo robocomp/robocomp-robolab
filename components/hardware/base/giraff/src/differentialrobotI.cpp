ed107c812edd2cffe13a9da92638ad917bd8af5b
pbustos <pbustos@unex.es>
2021-06-14 10:53:45 +0200

    created giraff base componente

---
+++ components/hardware/base/giraff/src/differentialrobotI.cpp
@@ -0,0 +1,71 @@
+/*
+ *    Copyright (C) 2021 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "differentialrobotI.h"
+
+DifferentialRobotI::DifferentialRobotI(GenericWorker *_worker)
+{
+	worker = _worker;
+}
+
+
+DifferentialRobotI::~DifferentialRobotI()
+{
+}
+
+
+void DifferentialRobotI::correctOdometer(int x, int z, float alpha, const Ice::Current&)
+{
+	worker->DifferentialRobot_correctOdometer(x, z, alpha);
+}
+
+void DifferentialRobotI::getBasePose(int &x, int &z, float &alpha, const Ice::Current&)
+{
+	worker->DifferentialRobot_getBasePose(x, z, alpha);
+}
+
+void DifferentialRobotI::getBaseState(RoboCompGenericBase::TBaseState &state, const Ice::Current&)
+{
+	worker->DifferentialRobot_getBaseState(state);
+}
+
+void DifferentialRobotI::resetOdometer(const Ice::Current&)
+{
+	worker->DifferentialRobot_resetOdometer();
+}
+
+void DifferentialRobotI::setOdometer(RoboCompGenericBase::TBaseState state, const Ice::Current&)
+{
+	worker->DifferentialRobot_setOdometer(state);
+}
+
+void DifferentialRobotI::setOdometerPose(int x, int z, float alpha, const Ice::Current&)
+{
+	worker->DifferentialRobot_setOdometerPose(x, z, alpha);
+}
+
+void DifferentialRobotI::setSpeedBase(float adv, float rot, const Ice::Current&)
+{
+	worker->DifferentialRobot_setSpeedBase(adv, rot);
+}
+
+void DifferentialRobotI::stopBase(const Ice::Current&)
+{
+	worker->DifferentialRobot_stopBase();
+}
+

73194d170a8c00f8a68eb41f5838523ecdcf9ebb
alfiTH <alex_alfi@hotmail.com>
2021-07-14 13:14:30 +0200

    new visual

---
+++ components/viewers/camera_simple_viewer/src/specificworker.cpp
@@ -0,0 +1,108 @@
+/*
+ *    Copyright (C) 2021 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(TuplePrx tprx, bool startup_check) : GenericWorker(tprx)
+{
+	this->startup_check_flag = startup_check;
+}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{
+	std::cout << "Destroying SpecificWorker" << std::endl;
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+//	THE FOLLOWING IS JUST AN EXAMPLE
+//	To use innerModelPath parameter you should uncomment specificmonitor.cpp readConfig method content
+//	try
+//	{
+//		RoboCompCommonBehavior::Parameter par = params.at("InnerModelPath");
+//		std::string innermodel_path = par.value;
+//		innerModel = std::make_shared(innermodel_path);
+//	}
+//	catch(const std::exception &e) { qFatal("Error reading config params"); }
+
+
+
+
+
+
+	return true;
+}
+
+void SpecificWorker::initialize(int period)
+{
+	std::cout << "Initialize worker" << std::endl;
+	this->Period = period;
+	if(this->startup_check_flag)
+	{
+		this->startup_check();
+	}
+	else
+	{
+		timer.start(Period);
+	}
+
+}
+
+void SpecificWorker::compute()
+{
+	try
+	{
+		auto image = this->camerasimple_proxy->getImage();
+
+		cv::Mat frame(cv::Size(image.width, image.height), CV_8UC3, &image.image[0], cv::Mat::AUTO_STEP);
+        cv::imshow("RGB image", frame);
+        cv::waitKey(1);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	
+	
+}
+
+int SpecificWorker::startup_check()
+{
+	std::cout << "Startup check" << std::endl;
+	QTimer::singleShot(200, qApp, SLOT(quit()));
+	return 0;
+}
+
+
+
+
+/**************************************/
+// From the RoboCompCameraSimple you can call this methods:
+// this->camerasimple_proxy->getImage(...)
+
+/**************************************/
+// From the RoboCompCameraSimple you can use this types:
+// RoboCompCameraSimple::TImage
+

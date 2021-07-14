6a85caca0d481fce86bbe386d078de8a2d7e8c89
alfiTH <alex_alfi@hotmail.com>
2021-07-14 14:55:26 +0200

    working

---
+++ components/hardware/camera/usb_camera/src/specificworker.cpp
@@ -0,0 +1,85 @@
+/*
+ *    Copyright (C) 2021 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(TuplePrx tprx, bool startup_check) : GenericWorker(tprx)
+{
+	this->startup_check_flag = startup_check;
+}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{
+	std::cout << "Destroying SpecificWorker" << std::endl;
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+
+	return true;
+}
+
+void SpecificWorker::initialize(int period)
+{
+	std::cout << "Initialize worker" << std::endl;
+
+	if( auto success = capture.open("/dev/video0"); success != true)
+    {
+	    qWarning() << __FUNCTION__ << " No camera found";
+	    std::terminate();
+    }
+
+	this->Period = 50;
+	if(this->startup_check_flag)
+		this->startup_check();
+	else
+		timer.start(Period);
+}
+
+void SpecificWorker::compute()
+{
+    capture >> frame;
+    cv::imshow("USB Camera", frame);
+    cv::waitKey(2); // waits to display frame
+}
+/////////////////////////////////////////////////////////////////////
+
+RoboCompCameraSimple::TImage SpecificWorker::CameraSimple_getImage()
+{
+
+
+}
+
+
+///////////////////////////////////////////////////////////////////77
+int SpecificWorker::startup_check()
+{
+	std::cout << "Startup check" << std::endl;
+	QTimer::singleShot(200, qApp, SLOT(quit()));
+	return 0;
+}
+
+
+
+

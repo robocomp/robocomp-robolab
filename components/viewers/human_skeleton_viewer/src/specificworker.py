398fb9625e03b5a812e439bfe0a69aa7abfd5c7e
pbustos <pbustos@unex.es>
2022-08-30 19:50:57 +0200

    working

--- components/viewers/human_skeleton_viewer/src/specificworker.py
+++ components/viewers/human_skeleton_viewer/src/specificworker.py
@@ -27,6 +27,7 @@ import interfaces as ifaces
 import cv2
 import traceback
 import numpy as np
+import time
 
 sys.path.append('/opt/robocomp/lib')
 console = Console(highlight=False)
@@ -34,18 +35,24 @@ console = Console(highlight=False)
 class SpecificWorker(GenericWorker):
     def __init__(self, proxy_map, startup_check=False):
         super(SpecificWorker, self).__init__(proxy_map)
-        self.Period = 50
+        self.Period = 33
         if startup_check:
             self.startup_check()
         else:
             ########## user code ##################
+            # Hz
+            self.cont = 0
+            self.last_time = time.time()
+
             try:
-                joint_data = self.humancamerabody_proxy.getJointData()
+                self.joint_data = self.humancamerabody_proxy.getJointData()
             except Ice.Exception:
                 traceback.print_exc()
                 print("Error connecting with HumanCameraBody to read TJointData")
                 sys.exit()
-            print(joint_data.jointNames)
+            print("Connected to HumanCameraBody")
+            print("Joint names: ", self.joint_data.jointNames)
+            #print("Connections:", self.joint_data.connections)
             ######################################
 
             self.timer.timeout.connect(self.compute)
@@ -64,21 +71,41 @@ class SpecificWorker(GenericWorker):
             img = self.camerargbdsimple_proxy.getImage('camera_top')
             color = np.frombuffer(img.image, dtype=np.uint8)
             color = color.reshape((img.height, img.width, 3))
-            cv2.imshow("Jetson", color)
-            cv2.waitKey(1)  # 1 millisecond
+        except Ice.Exception as e:
+            traceback.print_exc()
+
+        try:
+            people = self.humancamerabody_proxy.newPeopleData()
+            self.draw_people(people, color)
 
         except Ice.Exception as e:
             traceback.print_exc()
 
-        # try:
-        #     people = self.humancamerabody_proxy.newPeopleData()
-        # except Ice.Exception as e:
-        #     print(e)
+        cv2.imshow("Jetson", color)
+        cv2.waitKey(1)  # 1 millisecond
 
-
+        # FPS
+        self.show_fps()
         return True
 
-##############################################################################################
+    ##############################################################################################
+    def draw_people(self, people, img: np.ndarray):
+        for person in people.peoplelist:
+            for conn in self.joint_data.connections:
+                try:
+                    a = self.joint_data.jointNames[conn.first]
+                    b = self.joint_data.jointNames[conn.second]
+                    if (a in person.joints and b in person.joints):
+                        cv2.line(img, (person.joints[a].i, person.joints[a].j),
+                                 (person.joints[b].i, person.joints[b].j),
+                                 (0, 128, 0), 4)
+                except:
+                    pass
+
+            for id, jnt in person.joints.items():
+                cv2.circle(img, (jnt.i, jnt.j), 3, (255, 255, 255), 2)
+
+    ##############################################################################################
     def startup_check(self):
         print(f"Testing RoboCompCameraRGBDSimple.TImage from ifaces.RoboCompCameraRGBDSimple")
         test = ifaces.RoboCompCameraRGBDSimple.TImage()
@@ -98,6 +125,13 @@ class SpecificWorker(GenericWorker):
         test = ifaces.RoboCompHumanCameraBody.PeopleData()
         QTimer.singleShot(200, QApplication.instance().quit)
 
+    def show_fps(self):
+        if time.time() - self.last_time > 1:
+            self.last_time = time.time()
+            print("Freq: ", self.cont, "Hz. Waiting for image")
+            self.cont = 0
+        else:
+            self.cont += 1
     ######################
     # From the RoboCompCameraRGBDSimple you can call this methods:
     # self.camerargbdsimple_proxy.getAll(...)

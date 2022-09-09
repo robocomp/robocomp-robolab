b105bc4683316c3f725f0c313e56a3f1d89b446a
pbustos <pbustos@unex.es>
2022-09-09 15:35:35 +0200

    working firts steps

--- components/detection/jetson_bytetrack_tracker/src/specificworker.py
+++ components/detection/jetson_bytetrack_tracker/src/specificworker.py
@@ -24,26 +24,115 @@ from PySide2.QtWidgets import QApplication
 from rich.console import Console
 from genericworker import *
 import interfaces as ifaces
+import queue
+import traceback
+from threading import Thread
+import os
+import os.path as osp
+import time
+import cv2
+import torch
+from loguru import logger
+import numpy as np
+
+sys.path.append('/home/robocomp/software/ByteTrack')
+from yolox.data.data_augment import preproc
+from yolox.exp import get_exp
+from yolox.utils import fuse_model, get_model_info, postprocess
+from yolox.utils.visualize import plot_tracking
+from yolox.tracker.byte_tracker import BYTETracker
+from yolox.tracking_utils.timer import Timer
 
 sys.path.append('/opt/robocomp/lib')
 console = Console(highlight=False)
 
+class Predictor(object):
+    def __init__(
+        self,
+        model,
+        exp,
+        trt_file=None,
+        decoder=None,
+        device=torch.device("cuda"),
+        fp16=False
+    ):
+        self.model = model
+        self.decoder = decoder
+        self.num_classes = exp.num_classes
+        self.confthre = exp.test_conf
+        self.nmsthre = exp.nmsthre
+        self.test_size = exp.test_size
+        self.device = device
+        self.fp16 = fp16
+        if trt_file is not None:
+            from torch2trt import TRTModule
+            model_trt = TRTModule()
+            model_trt.load_state_dict(torch.load(trt_file))
+            x = torch.ones((1, 3, exp.test_size[0], exp.test_size[1]), device=device)
+            self.model(x)
+            self.model = model_trt
+        self.rgb_means = (0.485, 0.456, 0.406)
+        self.std = (0.229, 0.224, 0.225)
 
-# If RoboComp was compiled with Python bindings you can use InnerModel in Python
-# import librobocomp_qmat
-# import librobocomp_osgviewer
-# import librobocomp_innermodel
+    def inference(self, img, timer):
+        img_info = {"id": 0}
+        height, width = img.shape[:2]
+        img_info["height"] = height
+        img_info["width"] = width
+        img_info["raw_img"] = img
+
+        img, ratio = preproc(img, self.test_size, self.rgb_means, self.std)
+        img_info["ratio"] = ratio
+        img = torch.from_numpy(img).unsqueeze(0).float().to(self.device)
+        if self.fp16:
+            img = img.half()  # to FP16
+
+        with torch.no_grad():
+            timer.tic()
+            outputs = self.model(img)
+            if self.decoder is not None:
+                outputs = self.decoder(outputs, dtype=outputs.type())
+            outputs = postprocess(outputs, self.num_classes, self.confthre, self.nmsthre)
+        return outputs, img_info
 
 
 class SpecificWorker(GenericWorker):
     def __init__(self, proxy_map, startup_check=False):
         super(SpecificWorker, self).__init__(proxy_map)
-        self.Period = 2000
+        self.Period = 1
         if startup_check:
             self.startup_check()
         else:
+
+            self.exp = get_exp('/home/robocomp/software/ByteTrack/exps/example/mot/yolox_s_mix_det.py', "")
+            device = torch.device("cuda")
+            model = self.exp.get_model().to(device)
+            print("Model Summary: {}".format(get_model_info(model, self.exp.test_size)))
+            model.eval()
+            trt_file = "/home/nvidia/software/ByteTrack/YOLOX_outputs/yolox_s_mix_det/model_trt.pth"
+            assert osp.exists(trt_file), "TensorRT model is not found!\n Run python3 tools/trt.py first!"
+            model.head.decode_in_inference = False
+            decoder = model.head.decode_outputs
+            print("Using TensorRT to inference")
+            self.predictor = Predictor(model, self.exp, trt_file, decoder, device, False)
+            self.tracker = BYTETracker(frame_rate=60)
+
+            # camera read thread
+            self.read_queue = queue.Queue(1)
+            self.read_thread = Thread(target=self.get_rgb_thread, args=["camera_top"], name="read_queue", daemon=True)
+            self.read_thread.start()
+
+            # result data
+            self.objects_write = ifaces.RoboCompYoloObjects.TData()
+            self.objects_read = ifaces.RoboCompYoloObjects.TData()
+            
             self.timer.timeout.connect(self.compute)
             self.timer.start(self.Period)
+
+            # Hz
+            self.cont = 0
+            self.last_time = time.time()
+            self.fps = 0
 
     def __del__(self):
         """Destructor"""
@@ -54,28 +143,58 @@ class SpecificWorker(GenericWorker):
         # except:
         #	traceback.print_exc()
         #	print("Error reading config params")
+        print("Config params: ", params)
         return True
 
 
     @QtCore.Slot()
     def compute(self):
-        print('SpecificWorker.compute...')
-        # computeCODE
-        # try:
-        #   self.differentialrobot_proxy.setSpeedBase(100, 0)
-        # except Ice.Exception as e:
-        #   traceback.print_exc()
-        #   print(e)
+        frame = self.read_queue.get()
 
-        # The API of python-innermodel is not exactly the same as the C++ version
-        # self.innermodel.updateTransformValues('head_rot_tilt_pose', 0, 0, 0, 1.3, 0, 0)
-        # z = librobocomp_qmat.QVec(3,0)
-        # r = self.innermodel.transform('rgbd', z, 'laser')
-        # r.printvector('d')
-        # print(r[0], r[1], r[2])
+        timer = Timer()
+        frame_id = 0
+        results = []
+        outputs, img_info = self.predictor.inference(frame, timer)
+        if outputs[0] is not None:
+            online_targets = self.tracker.update(outputs[0], [frame.shape[0], frame.shape[1]], self.exp.test_size)
+            online_tlwhs = []
+            online_ids = []
+            online_scores = []
+            for t in online_targets:
+                tlwh = t.tlwh
+                tid = t.track_id
+                vertical = tlwh[2] / tlwh[3] > 1.6
+                if tlwh[2] * tlwh[3] > 10 and not vertical:
+                    online_tlwhs.append(tlwh)
+                    online_ids.append(tid)
+                    online_scores.append(t.score)
+                    results.append(
+                        f"{frame_id},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{t.score:.2f},-1,-1,-1\n"
+                    )
+            timer.toc()
+            frame = plot_tracking(frame, online_tlwhs, online_ids, frame_id=frame_id + 1, fps=1. / timer.average_time)
+        else:
+            timer.toc()
 
+        cv2.imshow('ByteTrack', frame)
+        cv2.waitKey(1)
+        
         return True
 
+    ##########################################################################################
+    def get_rgb_thread(self, camera_name: str):
+        while True:
+            try:
+                rgb = self.camerargbdsimple_proxy.getImage(camera_name)
+                frame = np.frombuffer(rgb.image, dtype=np.uint8)
+                frame = frame.reshape((rgb.height, rgb.width, 3))
+                self.read_queue.put(frame)
+            except:
+                print("Error communicating with CameraRGBDSimple")
+                traceback.print_exc()
+                return
+
+    ###########################################################################################
     def startup_check(self):
         print(f"Testing RoboCompCameraRGBDSimple.TImage from ifaces.RoboCompCameraRGBDSimple")
         test = ifaces.RoboCompCameraRGBDSimple.TImage()
@@ -106,7 +225,7 @@ class SpecificWorker(GenericWorker):
     # IMPLEMENTATION of getImage method from YoloObjects interface
     #
     def YoloObjects_getImage(self):
-        ret = RoboCompYoloObjects.RoboCompCameraRGBDSimple::TImage()
+        ret = ifaces.RoboCompYoloObjects.RoboCompCameraRGBDSimple.TImage()
         #
         # write your CODE here
         #

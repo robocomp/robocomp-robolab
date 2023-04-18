dcac6980dddd9f7ce13182aa2c13c2fd93fb0f10
alfiTH <alex_alfi@hotmail.com>
2023-04-18 17:55:25 +0200

    add lidar3d viewer

---
+++ components/viewers/lidar3d_viewer/src/specificworker.cpp
@@ -0,0 +1,123 @@
+/*
+ *    Copyright (C) 2023 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+#include <cppitertools/enumerate.hpp>
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(TuplePrx tprx, bool startup_check) : GenericWorker(tprx)
+{
+	this->startup_check_flag = startup_check;
+	// Uncomment if there's too many debug messages
+	// but it removes the possibility to see the messages
+	// shown in the console with qDebug()
+//	QLoggingCategory::setFilterRules("*.debug=false\n");
+}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{
+	std::cout << "Destroying SpecificWorker" << std::endl;
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+//	THE FOLLOWING IS JUST AN EXAMPLE
+//	To use innerModelPath parameter you should uncomment specificmonitor.cpp readConfig method content
+//	try
+//	{
+//		RoboCompCommonBehavior::Parameter par = params.at("InnerModelPath");
+//		std::string innermodel_path = par.value;
+//		innerModel = std::make_shared(innermodel_path);
+//	}
+//	catch(const std::exception &e) { qFatal("Error reading config params"); }
+
+	return true;
+}
+
+void SpecificWorker::initialize(int period)
+{
+	std::cout << "Initialize worker" << std::endl;
+	this->Period = period;
+	if(this->startup_check_flag)
+	{
+		this->startup_check();
+	}
+	else
+	{
+		// 3DViewer
+        points = std::make_shared<std::vector<std::tuple<float, float, float>>>();
+        colors = std::make_shared<std::vector<std::tuple<float, float, float>>>();
+        
+        viewer_3d = new Viewer(this, points, colors);
+        viewer_3d->show();
+
+		timer.start(Period);
+	}
+}
+
+void SpecificWorker::compute()
+{
+	try
+	{
+		points->clear(); colors->clear();
+		auto ldata = lidar3d_proxy->getLidarData();
+		qInfo() << ldata.size();
+		points->resize(ldata.size());
+		colors->resize(points->size());
+		
+	    for(const auto &[i, p]: ldata | iter::enumerate)
+		{
+			points->operator[](i) = std::make_tuple(p.x, p.y, p.z);
+			colors->operator[](i) = std::make_tuple(0, 0, 1);
+		}
+
+		viewer_3d->updateGL();
+
+	}
+	catch(const Ice::Exception &e)
+	{
+	  std::cout << "Error reading from Lidar3D" << e << std::endl;
+	}
+
+}
+
+int SpecificWorker::startup_check()
+{
+	std::cout << "Startup check" << std::endl;
+	QTimer::singleShot(200, qApp, SLOT(quit()));
+	return 0;
+}
+
+
+
+
+
+
+/**************************************/
+// From the RoboCompLidar3D you can call this methods:
+// this->lidar3d_proxy->getLidarData(...)
+
+/**************************************/
+// From the RoboCompLidar3D you can use this types:
+// RoboCompLidar3D::TPoint
+
---
+++ components/viewers/lidar3d_viewer/src/specificworker.h
@@ -0,0 +1,108 @@
+/*
+ *    Copyright (C) 2023 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+	\brief
+	@author authorname
+*/
+
+
+
+#ifndef SPECIFICWORKER_H
+#define SPECIFICWORKER_H
+
+#include <genericworker.h>
+#include <QGLViewer/qglviewer.h>
+#include <cppitertools/zip.hpp>
+
+using point3f = std::tuple<float, float, float>;
+
+class Viewer : public QGLViewer
+{
+    public:
+        Viewer(QWidget *parent_, std::shared_ptr<std::vector<std::tuple<float, float, float>>> points_,
+                                 std::shared_ptr<std::vector<std::tuple<float, float, float>>> colors_) : QGLViewer(parent_)
+        {
+            parent = parent_;
+            resize(parent->width(), parent->height());
+            points = points_;
+            colors = colors_;
+        };
+        ~Viewer(){ saveStateToFile(); };
+
+    protected:
+        virtual void draw()
+        {
+            drawAxis();
+            glColor3f(0.7, 0.7, 0.7);
+            drawGrid(5.0, 10);
+            glBegin(GL_POINTS);
+                for (auto &&[point, color] : iter::zip(*points, *colors))
+                {
+                    auto &[x,y,z] = point;
+                    auto &[r,g,b] = color;
+                    glColor3f(r, g, b);
+                    glVertex3f(x, y, z);
+                }
+            glEnd();
+            resize(parent->width(), parent->height());  // move to a signal
+        };
+        virtual void init()
+        {
+            restoreStateFromFile();
+            glDisable(GL_LIGHTING);
+            glPointSize(3.0);
+            setSceneRadius(5.0);
+        };
+        virtual void animate(){};
+        virtual QString helpString() const { return QString();};
+
+    private:
+        std::shared_ptr<std::vector<std::tuple<float, float, float>>> points, colors;
+        QWidget *parent;
+};
+
+
+
+
+
+class SpecificWorker : public GenericWorker
+{
+    Q_OBJECT
+    public:
+        SpecificWorker(TuplePrx tprx, bool startup_check);
+        ~SpecificWorker();
+        bool setParams(RoboCompCommonBehavior::ParameterList params);
+
+    public slots:
+        void compute();
+        int startup_check();
+        void initialize(int period);
+    private:
+        bool startup_check_flag;
+
+        Viewer *viewer_3d;
+        std::shared_ptr<std::vector<point3f>> points, colors;
+        std::shared_ptr<std::vector<std::tuple<point3f, point3f, point3f, point3f>>> planes;
+
+
+
+};
+
+#endif

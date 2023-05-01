0c522958faee45559ed01c35f714f3f6ae378c30
pbustos <pbustos@unex.es>
2023-05-01 23:40:53 +0200

    added viewer 360

---
+++ components/viewers/camera_360_viewer/src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Comment out this line if your application has a QtGui
+//#define USE_QTGUI
+
+
+#define PROGRAM_NAME    "Camera360Viewer"
+#define SERVER_FULL_NAME   "RoboComp Camera360Viewer:: Camera360Viewer"
+
+#endif

401b3e2d9d660464f9386bc8231f137ca8bce7e2
alfiTH <alex_alfi@hotmail.com>
2023-10-16 13:27:30 +0200

    update new robocomp camera_simple_viewer

--- components/viewers/camera_simple_viewer/src/genericworker.cpp
+++ components/viewers/camera_simple_viewer/src/genericworker.cpp
@@ -25,7 +25,7 @@ GenericWorker::GenericWorker(TuplePrx tprx) : QObject()
 
 	camerasimple_proxy = std::get<0>(tprx);
 
-	mutex = new QMutex(QMutex::Recursive);
+	mutex = new QMutex();
 
 	Period = BASIC_PERIOD;
 	connect(&timer, SIGNAL(timeout()), this, SLOT(compute()));
